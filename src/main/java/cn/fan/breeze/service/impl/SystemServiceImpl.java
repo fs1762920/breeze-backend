0ce9edaf6fefc1756da62b213d5040a2f0f4f19f
fanshuai <fs1762920@163.com>
2021-11-20 11:05:09 +0800

    网站信息接口优化

--- src/main/java/cn/fan/breeze/service/impl/SystemServiceImpl.java
+++ src/main/java/cn/fan/breeze/service/impl/SystemServiceImpl.java
@@ -14,6 +14,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
 
 @Service
 @Slf4j
@@ -46,36 +48,65 @@ public class SystemServiceImpl implements SystemService {
     public Map<String, Object> websiteInfo() {
         Map<String, Object> result = new HashMap<>();
 
-        //博客数量
-        Integer blogCount = blogMapper.getCount();
-        result.put("blogCount", blogCount);
-        //评论数量
-        Integer commentCount = commentMapper.getCount();
-        Integer replyCount = replyMapper.getCount();
-        result.put("commentCount", commentCount + replyCount);
-        //阅读量
-        Integer readingCount = blogMapper.getReadingCount();
-        result.put("readingCount", readingCount);
-        //建站时长
-        List<UserEntity> userEntityList = userMapper.selectBySelective(new UserEntity());
-        if (userEntityList.size() == 1) {
-            Date buildTime = userEntityList.get(0).getCtime();
-            result.put("buildTime", format.format(buildTime));
-            long currentMillion = new Date().getTime();
-            long buildDurationMillion = currentMillion - buildTime.getTime();
-            long buildDuration = buildDurationMillion / (1000 * 60 * 60 * 24);
-            result.put("buildDuration", buildDuration);
-        }
-        //分类数量
-        Integer classifyCount = classifyMapper.getCount();
-        result.put("classifyCount", classifyCount);
-        //标签数量
-        Integer labelCount = labelMapper.getCount();
-        result.put("labelCount", labelCount);
-        //友链数量
-        Integer friendCount = friendMapper.getCount();
-        result.put("friendCount", friendCount);
+        CompletableFuture<String> blogCountFuture = CompletableFuture.supplyAsync(() -> {
+            Integer blogCount = blogMapper.getCount();
+            result.put("blogCount", blogCount);
+            return "success";
+        });
 
+        CompletableFuture<String> commentCountFuture = CompletableFuture.supplyAsync(() -> {
+            //评论数量
+            Integer commentCount = commentMapper.getCount();
+            Integer replyCount = replyMapper.getCount();
+            result.put("commentCount", commentCount + replyCount);
+            return "success";
+        });
+
+        CompletableFuture<String> readingCountFuture = CompletableFuture.supplyAsync(() -> {
+            //阅读量
+            Integer readingCount = blogMapper.getReadingCount();
+            result.put("readingCount", readingCount);
+            return "success";
+        });
+
+        CompletableFuture<String> buildDurationFuture = CompletableFuture.supplyAsync(() -> {
+            //建站时长
+            List<UserEntity> userEntityList = userMapper.selectBySelective(new UserEntity());
+            if (userEntityList.size() == 1) {
+                Date buildTime = userEntityList.get(0).getCtime();
+                result.put("buildTime", format.format(buildTime));
+                long currentMillion = new Date().getTime();
+                long buildDurationMillion = currentMillion - buildTime.getTime();
+                long buildDuration = buildDurationMillion / (1000 * 60 * 60 * 24);
+                result.put("buildDuration", buildDuration);
+            }
+            return "success";
+        });
+
+        CompletableFuture<String> classifyCountFuture = CompletableFuture.supplyAsync(() -> {
+            //分类数量
+            Integer classifyCount = classifyMapper.getCount();
+            result.put("classifyCount", classifyCount);
+            return "success";
+        });
+
+        CompletableFuture<String> labelCountFuture = CompletableFuture.supplyAsync(() -> {
+            //标签数量
+            Integer labelCount = labelMapper.getCount();
+            result.put("labelCount", labelCount);
+            return "success";
+        });
+
+        CompletableFuture<String> friendCountFuture = CompletableFuture.supplyAsync(() -> {
+            //友链数量
+            Integer friendCount = friendMapper.getCount();
+            result.put("friendCount", friendCount);
+            return "success";
+        });
+
+        CompletableFuture<Void> allFuture = CompletableFuture.allOf(blogCountFuture, commentCountFuture, readingCountFuture,
+                buildDurationFuture, classifyCountFuture, labelCountFuture, friendCountFuture);
+        allFuture.join();
         return result;
     }
 }
